761bed9c454a0d9768e9f9e35fc58ab22aacc8a8
uche onuekwusi <ucheonuekwusi1234@gmail.com>
2019-11-20 04:12:37 +0000

    final draft checking typographical errors and css

--- app.js
+++ app.js
@@ -1,38 +1,52 @@
 let starships;
 const button = document.querySelector(".button");
-const input = document.querySelector(".input");
 
 // render starships function in the ui
 function renderShips(torender) {
+  const input = document.querySelector(".input");
   let entry = "";
   torender.forEach(element => {
+    parseInt(element.MGLT);
     entry += `<ul class="list"> `;
     entry +=
-      "<li>" + `<a href =${element.url} >` + element.name + ":" + "</li> </ul>";
+      "<li>" +
+      `<a href =${element.url} >` +
+      `<span class = "element_name">
+    ${element.name} 
+      </span>` +
+      " : " +
+      parseInt(input.value) / parseInt(element.MGLT) +
+      "</li> </ul>";
   });
 
   document.querySelector(".starships").innerHTML = entry;
 }
 
 button.addEventListener("click", () => {
-  //Using the  the API Endpoint above to retrieve the data
-  fetch("https://swapi.co/api/starships/")
-    .then(function(response) {
-      if (response.status !== 200) {
-        console.log(
-          "Looks like there was a problem. Status Code: " + response.status
-        );
-        return;
-      }
+  const input = document.querySelector(".input").value;
+  if (input === "") {
+    alert("Please use distance in mega lights (MGLT) in input box");
+  } else {
+    //Using the  the API Endpoint above to retrieve the data
 
-      // Examine the text in the response
-      response.json().then(function(data) {
-        console.log(data);
-        starships = data.results;
-        renderShips(starships);
+    fetch("https://swapi.co/api/starships/")
+      .then(function(response) {
+        if (response.status !== 200) {
+          console.log(
+            "Looks like there was a problem. Status Code: " + response.status
+          );
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function(data) {
+          console.log(data);
+          starships = data.results;
+          renderShips(starships);
+        });
+      })
+      .catch(function(err) {
+        console.log("Fetch Error :-S", err);
       });
-    })
-    .catch(function(err) {
-      console.log("Fetch Error :-S", err);
-    });
+  }
 });
